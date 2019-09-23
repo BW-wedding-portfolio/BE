14a720c6f5d62cba61a888ec46d4c4de0c6b638a
Nicholas Patterson <nicholaspatterson36@gmail.com>
2019-09-23 16:02:24 -0400

    :sparkles: adds auth router to server js and express json
    adds auth router to server js and express json

--- api/server.js
+++ api/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const authRouter = require("../auth/authRouter");
 const server = express();
 
 server.use(express.json());
+server.use("/auth", authRouter);
 
 module.exports = server;
