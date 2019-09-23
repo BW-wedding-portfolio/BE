d27a73a4b8dfb5262bf6bebfde526f2a41b8d251
Nicholas Patterson <nicholaspatterson36@gmail.com>
2019-09-23 18:59:50 -0400

    :sparkles: adds plannerRouter
    adds plannerRouter

--- api/server.js
+++ api/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const authRouter = require("../auth/authRouter");
+const plannerRouter = require("../planners/plannerRouter");
 const server = express();
 
 server.use(express.json());
 server.use("/auth", authRouter);
+server.use("/api", plannerRouter);
 
 module.exports = server;
