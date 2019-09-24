bb3ce2bbdd7f82bf85e4ea38a04f9e361eda777a
Nicholas Patterson <nicholaspatterson36@gmail.com>
2019-09-24 14:08:58 -0400

    :fire: removed routers that were not needed
    removed routers that were not needed

--- api/server.js
+++ api/server.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const authRouter = require("../auth/authRouter");
-const plannerRouter = require("../planners/plannerRouter");
-const portfolioRouter = require("../portfolios/portfolioRouter");
+const eventRouter = require("../events/eventRouter");
 const server = express();
 
 server.use(express.json());
 server.use("/auth", authRouter);
-server.use("/api", plannerRouter);
-server.use("/portfolio", portfolioRouter);
+server.use("/planner", eventRouter);
 
 module.exports = server;
