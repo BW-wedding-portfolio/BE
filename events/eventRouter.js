dcc8c10c62a0d7eb2a9250e99c43e0a1d02d5a67
Nicholas Patterson <nicholaspatterson36@gmail.com>
2019-09-24 15:30:07 -0400

    :sparkles: adds update and delete routes
    adds update and delete routes

--- events/eventRouter.js
+++ events/eventRouter.js
@@ -30,4 +30,47 @@ router.post("/:id/events", (req, res) => {
     .catch({ error: "Sever could not create event" });
 });
 
+router.put("/:id/events/:eid", (req, res) => {
+  const { id } = req.params;
+  let changes = req.body;
+  planner_id = id;
+  changes.planner_id = planner_id;
+  const { eid } = req.params;
+
+  Events.getEvent(eid)
+    .then(event => {
+      if (event) {
+        Events.update(changes, eid).then(updatedEvent => {
+          res.status(200).json(updatedEvent);
+        });
+      } else {
+        res.status(400).json({ error: "Could not find event with given id" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ message: "Failed to update event" });
+    });
+});
+
+router.delete("/:id/events/:eid", (req, res) => {
+  const { id } = req.params;
+  const { eid } = req.params;
+
+  let deleted = req.body;
+  planner_id = id;
+  deleted.planned_id = planner_id;
+
+  Events.remove(eid)
+    .then(deleted => {
+      if (deleted) {
+        res.status(200).json({ removed: deleted });
+      } else {
+        res.status(404).json({ error: "Could not find event with that id" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ error: "Server could not delete event" });
+    });
+});
+
 module.exports = router;
