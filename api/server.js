cb2556056b4eea67f01017e1912da1091d55afe3
Nicholas Patterson <nicholaspatterson36@gmail.com>
2019-09-24 17:39:02 -0400

    :sparkles: adds cors to server js
    adds cors to server js

--- api/server.js
+++ api/server.js
@@ -2,7 +2,9 @@ const express = require("express");
 const authRouter = require("../auth/authRouter");
 const eventRouter = require("../events/eventRouter");
 const server = express();
+const cors = require("cors");
 
+server.use(cors());
 server.use(express.json());
 server.use("/auth", authRouter);
 server.use("/planner", eventRouter);
